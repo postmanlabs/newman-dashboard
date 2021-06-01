07894603b1a01ccbeb45ad6a5bfe7097ba9ad55b
elit-altum <manan.sharma311@gmail.com>
2021-06-01 14:36:01 +0530

    add: broker server + static frontend

---
+++ bin/broker/server.js
@@ -0,0 +1,59 @@
+// establish express + socket.io server
+const express = require('express');
+const socket = require('socket.io');
+const path = require('path');
+
+// port for the dashboard
+const PORT = 5001;
+
+// initialize express app
+const app = express();
+
+// create express server
+const server = app.listen(PORT, () => {
+    if (process.hasOwnProperty('send')) {
+        // launched as a daemon: emit success message to parent process
+        process?.send({
+            status: 'success',
+            message: `\nSUCCESS: Dashboard is up at port ${PORT}.`,
+        });
+    } else {
+        // launched for dev: log success to console
+        console.log(`Dashboard is running at port: ${PORT}`);
+    }
+});
+
+// error while creating express server
+server.on('error', (err) => {
+    let serverError = '\nERROR: Cannot start dashboard.';
+
+    // if port is busy, create custom error
+    if (err.code === 'EADDRINUSE') {
+        serverError += `\nPort ${PORT} is busy. 
+        Please try again on another port.`;
+    } else {
+        serverError += `\n${err.message}`;
+    }
+
+    if (process.hasOwnProperty('send')) {
+        // launched as a daemon: emit failure message to parent process
+        process?.send({
+            status: 'fail',
+            message: serverError,
+        });
+    } else {
+        // launched for dev: log error to console
+        console.log(serverError);
+    }
+});
+
+// serve static files - for the frontend
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
+
+// setup socket.io server
+const io = socket(server);
+
+// event listener for a new connection
+io.on('connection', (client) => {
+    console.log(client);
+});
