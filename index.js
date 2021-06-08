0619de10d8e2849ccbe5fb274efd016d14e01b84
elit-altum <manan.sharma311@gmail.com>
2021-06-08 21:55:08 +0530

    add: --daemonize option to damonize server and --test for CLI unit tests

--- index.js
+++ index.js
@@ -12,15 +12,24 @@ program
 program
     .option(
         '-p, --port <port>',
-        'Specify the port to launch the dashboard',
+        'Specify the port to launch the dashboard.',
         '3000'
     )
+    .option('-d, --daemonize', 'Run the server as a daemon.')
+    .option('-t, --test', 'Run the CLI without any actions for unit tests.')
     .action(() => {
-        if (process.env.NODE_ENV !== 'test') {
+        if (program.opts().test) {
+            return;
+        }
+
+        if (program.opts().daemonize) {
             // launch the broker as a daemon
             const port = program.opts().port;
             launchBroker(port);
+            return;
         }
+
+        require('./bin/broker/server');
     })
     .parse(process.argv);
 
