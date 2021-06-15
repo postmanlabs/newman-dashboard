1c071c4bec9d46dd4ff80dddd4f89a4ba0d22e5b
elit-altum <manan.sharma311@gmail.com>
2021-06-15 14:36:10 +0530

    chore: fix .nycrc and comments to test files

--- test/unit-tests/broker-launch.test.js
+++ test/unit-tests/broker-launch.test.js
@@ -5,22 +5,29 @@ const expect = require('chai').expect;
 
 const launchBroker = require('../../dashboard/index');
 
+// mock event emitter to simulate ChildProcess object
 const mockChildProcess = new EventEmitter();
 
+// sinon fakes for behaviour verification
 let fake = sinon.fake();
 
 describe('Broker daemonized launching', () => {
     before(() => {
+        // stub cp.spawn, to avoid actual daemon launch
         sinon.stub(cp, 'spawn').callsFake(() => {
-            mockChildProcess.unref = fake;
-            mockChildProcess.disconnect = fake;
+            mockChildProcess['unref'] = fake;
+            mockChildProcess['disconnect'] = fake;
+
             return mockChildProcess;
         });
 
+        // stub process.exit to avoid exiting
         sinon.stub(process, 'exit');
+
+        // stub console.log for daemon status output
         sinon.stub(console, 'log');
 
-        launchBroker(3000);
+        launchBroker(5001);
     });
 
     after(() => {
@@ -31,6 +38,7 @@ describe('Broker daemonized launching', () => {
 
     it('should launch broker correctly', () => {
         mockChildProcess.emit('message', (cb) => cb());
+        // 2 calls to unref() and disconnect()
         expect(fake.callCount).to.equal(2);
     });
 });
