a9f818fe5f8167a89e41fd6abf30d581e6faa8ea
elit-altum <manan.sharma311@gmail.com>
2021-06-15 15:16:11 +0530

    add: tests to validate port number and spawn args

--- test/unit-tests/broker-launch.test.js
+++ test/unit-tests/broker-launch.test.js
@@ -10,14 +10,16 @@ const mockChildProcess = new EventEmitter();
 
 // sinon fakes for behaviour verification
 let fake = sinon.fake();
+let spawnArgs;
 
 describe('Broker daemonized launching', () => {
     before(() => {
         // stub cp.spawn, to avoid actual daemon launch
-        sinon.stub(cp, 'spawn').callsFake(() => {
+        sinon.stub(cp, 'spawn').callsFake((...args) => {
+            spawnArgs = args;
+
             mockChildProcess['unref'] = fake;
             mockChildProcess['disconnect'] = fake;
-
             return mockChildProcess;
         });
 
@@ -36,9 +38,24 @@ describe('Broker daemonized launching', () => {
         console.log.restore();
     });
 
+    it('should provide correct spawn arguments', () => {
+        expect(spawnArgs[0]).to.equal(process.execPath);
+        expect(spawnArgs[1]).to.eql(['./dashboard/server/index.js']);
+        expect(spawnArgs[2]).to.eql({
+            detached: true,
+            stdio: ['ignore', 'ignore', 'ignore', 'ipc'],
+        });
+    });
+
     it('should launch broker correctly', () => {
         mockChildProcess.emit('message', (cb) => cb());
+
         // 2 calls to unref() and disconnect()
         expect(fake.callCount).to.equal(2);
+    });
+
+    it('should not accept invalid port numbers', () => {
+        expect(() => launchBroker(99999)).to.throw('Invalid port number.');
+        expect(() => launchBroker(0)).to.throw('Invalid port number.');
     });
 });
