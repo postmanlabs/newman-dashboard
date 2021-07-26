bf47bf89bf38538e68f4ee57ae4010a851e0bbf6
elit-altum <manan.sharma311@gmail.com>
2021-07-26 12:22:18 +0530

    build: frontend with details page for every run

--- dashboard/frontend/out/_next/static/9ZPpBTOTaGnqL5ImERyGL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d59979e69b813386fb6b.js"],"/_error":["static/chunks/pages/_error-706fb6b4b5a8fdae0611.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
