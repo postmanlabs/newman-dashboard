e0fc92a5cd082a13eaae79da9053ad4a8731f413
elit-altum <manan.sharma311@gmail.com>
2021-08-28 21:16:41 +0530

    chore: change db dir name for dev mode

--- dashboard/lib/store/index.js
+++ dashboard/lib/store/index.js
@@ -1,12 +1,12 @@
-const connect = require('camo').connect;
-const uri = 'nedb://../data/index.db';
+const connect = require("camo").connect;
+const uri = "nedb://lib/data";
 
 const init = async () => {
-  try {
-    await connect(uri);
-  } catch (e) {
-    console.log('Error in connecting to database.')
-  }
-}
+    try {
+        await connect(uri);
+    } catch (e) {
+        console.log("Error in connecting to database.");
+    }
+};
 
 module.exports = { init };
