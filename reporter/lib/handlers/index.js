7b13c0bdac7cb65f02cfff8a23bcae96d0d4a757
elit-altum <manan.sharma311@gmail.com>
2021-09-02 21:22:55 +0530

    add: time of event emits for sorting

--- reporter/lib/handlers/index.js
+++ reporter/lib/handlers/index.js
@@ -63,6 +63,7 @@ module.exports = (socket, id) => {
                 socket.emit("run-event", {
                     type: event,
                     runId: id,
+                    time: Date.now(),
                     args,
                     err,
                 });
